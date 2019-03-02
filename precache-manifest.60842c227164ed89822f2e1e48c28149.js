351cc3a7ec34bad5ad6a06d58f2e570c0dfe7a53
Sara Borham <saraborham193@yahoo.com>
2019-03-02 22:40:54 +0200

    Updates

---
+++ precache-manifest.60842c227164ed89822f2e1e48c28149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/huddle-beginner/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "da836b3b87a950b3f621",
+    "url": "/huddle-beginner/static/js/runtime~main.da836b3b.js"
+  },
+  {
+    "revision": "99ec3e9823e812134a29",
+    "url": "/huddle-beginner/static/js/main.99ec3e98.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/huddle-beginner/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "99ec3e9823e812134a29",
+    "url": "/huddle-beginner/static/css/main.4bff8345.chunk.css"
+  },
+  {
+    "revision": "50ebb379077681064c5670d0ecd3288e",
+    "url": "/huddle-beginner/index.html"
+  }
+];
